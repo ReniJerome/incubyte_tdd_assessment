8736eca7df2426b5e8fdf1cb84b7fedb9043406e
ReniJerome <66509947+ReniJerome@users.noreply.github.com>
2024-11-24 12:12:52 +0530

    Update String_Calculator.java
    step 5 negative numbers

--- String_Calculator.java
+++ String_Calculator.java
@@ -18,15 +18,23 @@ public class Main
 		args1= args1.replace(",,",",");
 		System.out.println(args1);	
 	       int[] arr = Arrays.stream(args1.split(",")).mapToInt(Integer::parseInt).toArray();
+		      List<Integer> negative_numbers = new ArrayList<>[];
 
 		    int sum = 0;
 	            if(arr.length !=0)
 	            {
 	                for (int i=0;i<arr.length; i++)
 	                {
+				
 	                    sum += arr[i];
+			if (arr[1]<0)
+			{
+				negative_numbers.add(arr[i]);
 	                }
 	            }
+		if (!negative_numbers.isEmpty)
+			System.out.println("Negative numbers are "+negative_numbers);
+		else
 	            System.out.println("Sum is "+sum);
 	   }
 	   	catch(NumberFormatException e)
